d7907d036995d0b3d7029f003cedfa1b7018aab3
wakatakeru <shimizu.net421@gmail.com>
2016-07-10 01:19:42 +0900

    バグを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources: user
-  resources: image
+  resources :user
+  resources :image
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
