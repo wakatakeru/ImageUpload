2701da2d0e5131fcac5c3830fb2bb200b4d8fd7f
wakatakeru <shimizu.net421@gmail.com>
2016-07-10 01:34:56 +0900

    ルーティングのバグを修正

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,2 +1,14 @@
 class ImageController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    redirect_to :index
+  end
+  
 end
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  test "should get image" do
-    get :image
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
