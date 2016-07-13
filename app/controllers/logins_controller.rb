1222c8adb3fee64af5341854d59e1a819b7d7294
wakatakeru <shimizu.net421@gmail.com>
2016-07-13 21:22:48 +0900

    ログインのためのcontrollerを作成

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,2 +1,28 @@
+# coding: utf-8
 class LoginsController < ApplicationController
+  def index
+    render "index"
+  end
+
+  def show
+    render "new"
+  end
+
+  def create
+    user = User.find_by_name(params[:login_id])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now.alert = "認証エラー。もう一度入力してください"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @current_user = nil
+    redirect_to root_path
+  end
+
 end
