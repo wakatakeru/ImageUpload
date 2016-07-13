61a1918c71fa875333c02165e369c49e8d22717b
wakatakeru <shimizu.net421@gmail.com>
2016-07-13 15:36:16 +0900

    ユーザを新規作成できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new
+    user.login_id = params['user']['login_id']
+    user.email    = params['user']['email']
+    user.password = params['user']['password']
+    user.save
+
+    redirect_to images_path
+  end
+
+  def login
+    
+  end
   
   def show
     @user = "ほげほげ"
